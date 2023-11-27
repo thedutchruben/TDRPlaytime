14dcb715b412b5cee740e4feca3174b7ced5e879
Ruben de Roos <deroosruben@gmail.com>
2023-11-27 21:38:07 +0100

    Add compile script

--- scrips/compile.sh
+++ scrips/compile.sh
@@ -1,19 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
 # Define the array of values
-values=("GITHUB" "SPIGOT" "HANGAR")
+values=("SPIGOT" "HANGAR" "CURSE_FORGE" "MODRITH")
+mvn install
+
+# Specify the path to your pom.xml file
+pom_file="pom.xml"
+
+# Extract the version using grep and sed
+version=$(grep -m 1 -o '<version>[^<]*</version>' "$pom_file" | sed 's/<version>\(.*\)<\/version>/\1/')
+
+# Print the version
+echo "Creating Version: $version"
 
 # Loop through the array
 for value in "${values[@]}"; do
-    # Replace the previous value in plugin.yml
-    sed -i "s/GITHUB/$value/g" ../src/main/resources/plugin.yml
+    #!/bin/bash
 
-    # Compile the application with mvn install
-    mvn install
+    # Replace 'OLD_WORD' with the word you want to replace
+    OLD_WORD="GITHUB"
 
-    # Add the value to the filename
-#    mv target/your-application.jar target/your-application_$value.jar
+    # Specify the path to the input JAR file
+    INPUT_JAR="target/playtime-$version.jar"
 
-    # Reset plugin.yml to its original state for the next iteration
-    git checkout -- ../src/main/resources/plugin.yml
+    # Specify the path to the output JAR file
+    OUTPUT_JAR="target/playtime-$version-$value.jar"
+
+    # Extract the contents of the JAR
+    jar xf "$INPUT_JAR" plugin.yml
+
+    # Replace the word in the plugin.yml file
+    sed -i "s/$OLD_WORD/$value/g" plugin.yml
+
+    # Package the modified files back into a JAR
+    jar cf "$OUTPUT_JAR" plugin.yml
+
+    # Clean up temporary files
+    rm plugin.yml
+
 done
+
