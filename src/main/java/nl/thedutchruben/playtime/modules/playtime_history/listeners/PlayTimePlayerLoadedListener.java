1c7cc685d5202040eb104b4814481ffa651994b8
Ruben de Roos <deroosruben@gmail.com>
2025-01-02 16:57:59 +0100

    Update join and quit event for playtime history

--- src/main/java/nl/thedutchruben/playtime/modules/playtime_history/listeners/PlayTimePlayerLoadedListener.java
+++ src/main/java/nl/thedutchruben/playtime/modules/playtime_history/listeners/PlayTimePlayerLoadedListener.java
@@ -13,11 +13,11 @@ public class PlayTimePlayerLoadedListener implements Listener {
 
     @EventHandler
     public void onJoin(PlaytimePlayerLoadedEvent event) {
-        Playtime.getInstance().getStorage().updatePlaytimeHistory(event.getUser().getUUID(), Storage.Event.JOIN, (int) event.getUser().getTime());
+        Playtime.getInstance().getStorage().addPlaytimeHistory(event.getUser().getUUID(), Storage.Event.JOIN, (int) event.getUser().getTime());
     }
 
     @EventHandler
     public void onQuit(PlaytimePlayerUnLoadedEvent event) {
-        Playtime.getInstance().getStorage().updatePlaytimeHistory(event.getUser().getUUID(), Storage.Event.QUIT, (int) event.getUser().getTime());
+        Playtime.getInstance().getStorage().addPlaytimeHistory(event.getUser().getUUID(), Storage.Event.QUIT, (int) event.getUser().getTime());
     }
 }
